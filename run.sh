e684c90e29dd6ab162991a852d85f301f58aedb3
Enrico Daga <enridaga@apache.org>
2020-09-08 13:43:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=${1:-v0.6.5}
+version=${1:-v0.6.7}
 mongodb_version=${2:-v0.1}
 docker run --detach --name=apif-mongodb enridaga/apif-mongodb:$mongodb_version
 docker run --detach --env="VIRTUAL_HOST=apif.local" --name=apif-service --publish 82:80 enridaga/apif-service:$version
