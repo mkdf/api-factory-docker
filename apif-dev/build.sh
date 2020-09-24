a8166547b3f4ce6e602c3e1cedd56a2c4fd0e252
enridaga <enricodaga@gmail.com>
2020-09-24 14:40:40 +0100

    scripts updated

--- apif-dev/build.sh
+++ apif-dev/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -t enridaga/apif-service:$1 --build-arg APIF_VERSION=$1 -f Dockerfile .
+docker build -t enridaga/apif-service-dev:$1 --build-arg APIF_VERSION=$1 -f Dockerfile .
--- apif-dev/run.sh
+++ apif-dev/run.sh
@@ -2,4 +2,5 @@
 
 userdir=$(cd && pwd)
 # $2 is the folder of the api-factory project https://github.com/mkdf/api-factory
-docker run --detach --env="VIRTUAL_HOST=apif.local" --mount src="$2",target="/var/www/api-factory",type=bind --name=apif-service-dev enridaga/apif-service-dev:$1
+# $3 is the vhost
+docker run --detach --env="VIRTUAL_HOST=$3" --mount src="$2",target="/var/www/api-factory",type=bind --name=apif-service-dev enridaga/apif-service-dev:$1
